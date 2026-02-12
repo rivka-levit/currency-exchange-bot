fbc35ef0172610be703bd713b58da557b163942e
rivka-yuliia <rivka.developer@gmail.com>
2026-02-12 17:46:47 +0200

    base model class

---
+++ app/database/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    created: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
+    updated: Mapped[DateTime] = mapped_column(
+        DateTime,
+        default=func.now(),
+        onupdate=func.now()
+    )
