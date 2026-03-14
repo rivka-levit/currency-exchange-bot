8ce1a9ba58dc3d2d720fcc49ce14c540e4316621
rivka-yuliia <rivka.developer@gmail.com>
2026-03-14 21:55:48 +0200

    test i18n middleware user not in db

--- app/tests/test_middlewares/test_i18n_middleware.py
+++ app/tests/test_middlewares/test_i18n_middleware.py
@@ -50,3 +50,38 @@ async def test_i18n_middleware_returns_correct_object(lang, custom_user, chat):
         expected_i18n = translator(lang=lang)
         assert isinstance(i18n, LocalizedTranslator)
         assert i18n.get('start_answer') == expected_i18n.get('start_answer')
+
+
+async def test_i18n_middleware_no_db_user(custom_user, chat):
+    """Test i18n middleware with user not in database."""
+
+    middleware = FluentTranslatorMiddleware()
+    translator = Translator()
+    language = 'ru'
+    user = custom_user(language_code=language)
+    message = Message(
+        message_id=153,
+        date=datetime.now(),
+        chat=chat,
+        from_user=user
+    )
+    data = {
+        'event_from_user': user,
+        'session': AsyncMock(),
+        'translator': translator
+    }
+
+    with patch(
+            'middlewares.i18n.orm_get_user',
+            new_callable=AsyncMock,
+            return_value=None
+    ) as mock:
+        await middleware(next_handler, message, data)
+
+        assert 'i18n' in data
+        mock.assert_awaited_once_with(data['session'], user.id)
+
+        i18n = data['i18n']
+        expected_i18n = translator(lang=language)
+        assert isinstance(i18n, LocalizedTranslator)
+        assert i18n.get('help_answer') == expected_i18n.get('help_answer')
